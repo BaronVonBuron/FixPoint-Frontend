3efeae7657af5fad67110d1ae78ea2c7f5e227c8
BaronVonBuron <andbur01@easv365.dk>
2024-12-10 13:16:38 +0100

    Made the app show stuff actually there

--- src/progress-bar/progress-bar.component.ts
+++ src/progress-bar/progress-bar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {SelectedCaseService} from '../Services/selected.case.service';
 import {StatusPercentService} from '../Services/tools/status.percent.service';
 import {PriorityNamerService} from '../Services/tools/priority.namer.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-progress-bar',
@@ -16,8 +17,10 @@ export class ProgressBarComponent {
               private priorityNamerService: PriorityNamerService) {}
 
   public statusText: string = "";
+  public statusPercent?: number = 0;
 
   ngOnInit(): void {
     this.statusText = this.statusPercentService.getStatusMessage(this.selectedCaseService.getCase()?.status ?? 0);
+    this.statusPercent = this.statusPercentService.multiplyStatusBy25(this.selectedCaseService.getCase()?.status ?? 0);
   }
 }
