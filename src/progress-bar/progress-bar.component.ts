e6d24c2db970ab2a4ba323822fd1e6f4bfede311
BaronVonBuron <andbur01@easv365.dk>
2024-12-11 11:14:51 +0100

    progress bar is maybe done?

--- src/progress-bar/progress-bar.component.ts
+++ src/progress-bar/progress-bar.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import {SelectedCaseService} from '../Services/selected.case.service';
 import {StatusPercentService} from '../Services/tools/status.percent.service';
 import {PriorityNamerService} from '../Services/tools/priority.namer.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-progress-bar',
@@ -17,10 +16,40 @@ export class ProgressBarComponent {
               private priorityNamerService: PriorityNamerService) {}
 
   public statusText: string = "";
+  public arbejder: string = "#eee";
+  public reservedele: string = "#eee";
+  public klar: string = "#eee";
   public statusPercent?: number = 0;
 
   ngOnInit(): void {
     this.statusText = this.statusPercentService.getStatusMessage(this.selectedCaseService.getCase()?.status ?? 0);
     this.statusPercent = this.statusPercentService.multiplyStatusBy25(this.selectedCaseService.getCase()?.status ?? 0);
+    this.updateStatusColors(this.selectedCaseService.getCase()?.status ?? 0);
+  }
+
+  public updateStatusColors(status: number): void {
+    const activeColor = "#034c8c";
+    const defaultColor = "#eee";
+
+    // Reset all fields to default color
+    this.arbejder = defaultColor;
+    this.reservedele = defaultColor;
+    this.klar = defaultColor;
+
+    // Set colors based on the status
+    switch (status) {
+      case 2:
+        this.arbejder = activeColor;
+        break;
+      case 3:
+        this.arbejder = activeColor;
+        this.reservedele = activeColor;
+        break;
+      case 4:
+        this.arbejder = activeColor;
+        this.reservedele = activeColor;
+        this.klar = activeColor;
+        break;
+    }
   }
 }
